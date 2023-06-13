56b268ce327a2ddea089f73a1e33b806bda9ed2e
azim128 <azimruet28@gmail.com>
2023-06-13 16:23:25 +0600

    3rd

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+'use client'
+import { ThemeProvider } from 'next-themes';
+
+const Layout = ({ children }) => {
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+        { children }
+    </ThemeProvider>
+  )
+}
+
+export default Layout
